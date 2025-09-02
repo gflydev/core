c6a0b555585128caf6019c437f509e1881fc0ae8
Vinh Vo <vohuynhvinh@gmail.com>
2025-09-02 17:31:24 +0700

    Remove unused DTO structs and update the framework version.
    Deleted obsolete request and response DTO structs to streamline the codebase and remove redundancies. Additionally, updated the framework version from v1.17.3 to v1.17.4.

--- constants.go
+++ constants.go
@@ -7,7 +7,7 @@ package core
 // Version of the current gFly application
 const (
 	// Version indicates the version of the framework
-	Version = "v1.17.3"
+	Version = "v1.17.4"
 )
 
 // ====================================================================
