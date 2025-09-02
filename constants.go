643037b6164bad26dd851928f8faf54dcc82515b
Vinh Vo <vohuynhvinh@gmail.com>
2025-09-02 16:56:53 +0700

    Update application version to v1.17.3
    Incremented the version from v1.17.1 to v1.17.3 to reflect recent changes or fixes. This ensures version accuracy across the application.

--- constants.go
+++ constants.go
@@ -7,7 +7,7 @@ package core
 // Version of the current gFly application
 const (
 	// Version indicates the version of the framework
-	Version = "v1.17.1"
+	Version = "v1.17.3"
 )
 
 // ====================================================================
